7441c3d7459863522ccae9d4bdf03808f7a86b84
Alexandr123000 <alexandrt10000@gmail.com>
2026-07-19 21:13:43 +0300

    feat: add scroll-top-button button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-some text
+window.onscroll = function() {functionScroll()};
+
+function functionScroll()
+{
+	var button = document.getElementById("scroll-top-button");
+
+	if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500)
+	{
+		button.style.display = "block";
+	}
+	else
+	{
+		button.style.display = "none";
+	}
+}
+
+var btn = document.getElementById("scroll-top-button");
+
+function topFunction()
+{
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
